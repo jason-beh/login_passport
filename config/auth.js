11884da8c0cf3b159a5eaeb3c77fb4fd4cf609f0
Jason Beh Jie Shen <behjieshen@gmail.com>
2017-04-02 21:44:25 +0800

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -1,14 +1,14 @@
 module.exports = {
   'facebookAuth' : {
-    'clientID': '1725034947787605',
-    'clientSecret': '2d541bfac6ce22972a92023f39b7493f',
+    'clientID': '',
+    'clientSecret': '',
     'callbackURL': 'http://localhost:3000/auth/facebook/callback',
     'profileFields': ['id', 'displayName', 'email']
   },
 
   'googleAuth' : {
-    'clientID': '116630155539-d5mqln80bi5i6vreimis7aqr9madso77.apps.googleusercontent.com',
-    'clientSecret': 'zHcdV5VJqYT5feFwaDeoc0t4',
+    'clientID': '',
+    'clientSecret': '',
     'callbackURL': 'http://localhost:3000/auth/google/callback',
   }
 }
